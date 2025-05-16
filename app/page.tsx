9de9e7bf16068744a43df90f57492ad797dd9f9a
shree3105 <63193223+shree3105@users.noreply.github.com>
2025-05-16 10:25:18 +0100

    cleaned up unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
+
 import {
   Select,
   SelectTrigger,
